764cade6939f7f5e506a85d84679889a7c3fc6ee
Bruno Araujo <119737467+bruno-araujo-23@users.noreply.github.com>
2023-02-25 14:12:17 -0300

    estruturas de seleção

---
+++ exercicios_cap3_questao_07.py
@@ -0,0 +1,31 @@
+# Escreva um algoritmo que, a partir de um mês fornecido (1 a 12) apresente
+# o nome dele por extenso, ou mês inválido.
+
+mes = int(input("Digite o número do mês: "))
+
+if mes == 1:
+  print("January")
+elif mes == 2:
+  print("February")
+elif mes == 3:
+  print("March")
+elif mes == 4:
+  print("April")
+elif mes == 5:
+  print("May")
+elif mes == 6:
+  print("June")
+elif mes == 7:
+  print("July")
+elif mes == 8:
+  print("August")
+elif mes == 9:
+  print("September")
+elif mes == 10:
+  print("October")
+elif mes == 11:
+  print("November")
+elif mes == 12:
+  print("December")
+else:
+  print("invalid month")
