ec98485004ff7ca6c1f7196d45e455ce213d74e3
Bruno Araujo <119737467+bruno-araujo-23@users.noreply.github.com>
2023-02-25 11:21:29 -0300

    Create exercicios_cap3_questao_02.py

---
+++ exercicios_cap3_questao_02.py
@@ -0,0 +1,7 @@
+# Elabore um algoritmo que calcule a área de um círculo qualquer de raio fornecido
+
+raio = float(input("Digite o raio do círculo: "))
+pi = 3.14159
+area = pi*raio**2
+
+print(area)
